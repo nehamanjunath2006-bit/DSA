d7d48784f8c47078c03b13487d5099dfe0a5a48f
nehamanjunath2006-bit <nehamanjunath2006@gmail.com>
2026-05-28 16:40:04 +0530

    Create movezeroes.py

---
+++ LeetCode/movezeroes.py
@@ -0,0 +1,9 @@
+# optimized
+class Solution(object):
+    def moveZeroes(self, nums):
+        
+        for i in range(len(nums)):
+            for j in range(i + 1,len(nums)):
+                if nums[i] == 0 and nums[j] != 0:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    break
