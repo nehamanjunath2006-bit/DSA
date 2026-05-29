23ae75b549b3f54584f67e8c3a5d6dac37502d0d
nehamanjunath2006-bit <nehamanjunath2006@gmail.com>
2026-05-29 23:17:04 +0530

    Daily Update

--- LeetCode/anagram.py
+++ LeetCode/anagram.py
@@ -1,7 +1,12 @@
+# class Solution(object):
+#     def isAnagram(self, s, t):
+
+#         if len(s) != len(t):
+#             return False
+
+#         return sorted(s) == sorted(t)
 class Solution(object):
-    def isAnagram(self, s, t):
-
-        if len(s) != len(t):
-            return False
-
-        return sorted(s) == sorted(t)
+    def duplicates(self, nums):
+        print("enter the list of numbers:")
+        nums.sort()
+        return list(set(nums))
