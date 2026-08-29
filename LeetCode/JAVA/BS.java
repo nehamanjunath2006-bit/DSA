dc6dd13a227551ef6986f525fb7bbd651d7c8904
nehamanjunath2006-bit <nehamanjunath2006@gmail.com>
2026-08-29 20:03:29 +0530

    Daily Update

---
+++ LeetCode/JAVA/BS.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+class BS {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int first = -1;
+        int last = -1;
+
+        // Find first occurrence
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        // Find last occurrence
+        low = 0;
+        high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("First index: " + first);
+        System.out.println("Last index: " + last);
+    }
+}
